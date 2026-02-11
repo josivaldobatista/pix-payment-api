0a4bd1d246b5a1c92ab370198e6002220244d1f7
Josivaldo Batista <josivaldo-fb@outlook.com>
2026-02-11 11:03:59 -0300

    PPA-04/FEATURE: Adicionando tratamento de exceção para quando status != de PAID.

---
+++ src/main/java/com/jfb/pix/payment/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.jfb.pix.payment.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<StandardError> responseStatusException(
+            ResponseStatusException e,
+            HttpServletRequest request
+    ) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(
+                Instant.now(),
+                status.value(),
+                "Pagamento não está pendente",
+                e.getMessage(),
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(status).body(err);
+    }
+}
---
+++ src/main/java/com/jfb/pix/payment/exception/StandardError.java
@@ -0,0 +1,12 @@
+package com.jfb.pix.payment.exception;
+
+import java.time.Instant;
+
+public record StandardError(
+        Instant timestamp,
+        Integer status,
+        String error,
+        String message,
+        String path
+) {
+}
